5029434ff0b9cd7b624dea26a768743ef00a982d
khaledmalhal <khaled.malhal@estudiantat.upc.edu>
2023-12-11 01:55:58 +0100

    [Groupy] gms3 done, I think?

--- Groupy/gms3.erl
+++ Groupy/gms3.erl
@@ -1,32 +1,32 @@
 -module(gms3).
 -export([start/1, start/2]).
 -define(timeout, 1000).
--define(arghh, 100).
+-define(arghh, 10000000).
 
 start(Name) ->
     Self = self(),
-    spawn_link(fun()-> init(Name, Self, 0, none) end).
+    spawn_link(fun()-> init(Name, Self) end).
 
-init(Name, Master, N, Last) ->
+init(Name, Master) ->
     {A1,A2,A3} = erlang:timestamp(),
     random:seed(A1, A2, A3),
-    leader(Name, Master, [], N).
+    leader(Name, Master, [], 0).
 
 start(Name, Grp) ->
     Self = self(),
-    spawn_link(fun()-> init(Name, Grp, Self, 0, none) end).
+    spawn_link(fun()-> init(Name, Grp, Self) end).
 
-init(Name, Grp, Master, N, Last) ->
+init(Name, Grp, Master) ->
     {A1,A2,A3} = erlang:timestamp(),
     random:seed(A1, A2, A3),
     Self = self(),
-    Grp ! {join, Self, N, Last},
+    Grp ! {join, Self},
     receive
-        {view, Leader, Slaves, _} ->
+        {view, Leader, Slaves, Seq} ->
             io:format("[~s] Init. Monitoring the Leader: ~w~n", [Name, Leader]),
             MonitorRef = erlang:monitor(process, Leader),
             Master ! joined,
-            slave(Name, Master, Leader, Slaves, MonitorRef, N, Last)
+            slave(Name, Master, Leader, Slaves, MonitorRef, Seq, {view, Leader, Slaves, Seq})
     after ?timeout ->
         Master ! {error, "no reply from leader"}
     end.
@@ -37,23 +37,24 @@ election(Name, Master, Slaves, MonitorRef, N, Last) ->
         [Self|Rest] ->
             io:format("[~s] I am the new Leader. Pid: ~w~n", [Name, Self]),
             bcast(Name, {view, Self, Rest, N}, Rest),
-            leader(Name, Master, Rest, N + 1);
+            leader(Name, Master, Rest, N);
         [NewLeader|Rest] ->
             io:format("[~s] ~w is the new Leader~n", [Name, NewLeader]),
             slave(Name, Master, NewLeader, Rest, MonitorRef, N, Last)
     end.
 
 leader(Name, Master, Slaves, N) ->
+    io:format("Leader ~s. N: ~w~n", [Name, N]),
     receive
-        {mcast, Msg, _Seq} ->
+        {mcast, Msg} ->
             bcast(Name, {msg, Msg, N}, Slaves),
             Master ! {deliver, Msg},
             leader(Name, Master, Slaves, N + 1);
-        {join, Peer, _N, _Last} ->
+        {join, Peer} ->
             NewSlaves = lists:append(Slaves, [Peer]),
             io:format("Leader (~s): Peer wants to join (~w) ~n", [Name, Peer]),
             bcast(Name, {view, self(), NewSlaves, N}, NewSlaves),
-            leader(Name, Master, NewSlaves, N);
+            leader(Name, Master, NewSlaves, N + 1);
         stop ->
             ok;
         Error ->
@@ -73,28 +74,27 @@ crash(Name, Msg) ->
     end.
 
 slave(Name, Master, Leader, Slaves, MonitorRef, N, Last) ->
+    io:format("Slave ~s. N: ~w~n", [Name, N]),
     receive
-        {mcast, Msg, Seq} when Seq < N ->
-        %Ignora mensajes con numero de seqüencia repetidos
+        {mcast, Msg} ->
+            %Ignora mensajes con numero de secuencia repetidos
+            Leader ! {mcast, Msg},
             slave(Name, Master, Leader, Slaves, MonitorRef, N, Last);
-        {mcast, Msg, Seq} ->
-            Leader ! {mcast, Msg, Seq},
-            slave(Name, Master, Leader, Slaves, MonitorRef, N, Last);
-        {join, Peer, _N, _Last} ->
-            Leader ! {join, Peer, _N, _Last},
+        {join, Peer} ->
+            Leader ! {join, Peer},
             io:format("Slave (~s): Peer (~w) wants to join ~n", [Name, Peer]),
             slave(Name, Master, Leader, Slaves, MonitorRef, N, Last);
-        {msg, Msg, Seq} when Seq < N ->
-        %Ignora mensajes con numero de seqüencia repetidos
-            slave(Name, Master, Leader, Slaves, MonitorRef, N, Last);
-        {msg, Msg, Seq} ->
+        {msg, Msg, Seq} when Seq > N ->
+            io:format("~nSlave ~s. Received msg with Seq ~w.~n", [Name, Seq]),
+            %Ignora mensajes con numero de secuencia repetidos
             Master ! {deliver, Msg},
-            slave(Name, Master, Leader, Slaves, MonitorRef, N, Msg);
-        {view, NewLeader, NewSlaves, Seq} ->
+            slave(Name, Master, Leader, Slaves, MonitorRef, Seq, {msg, Msg, Seq});
+        {view, NewLeader, NewSlaves, Seq} when Seq > N ->
+            io:format("~nSlave ~s. Received view with Seq ~w.~n", [Name, Seq]),
             erlang:demonitor(MonitorRef, [flush]),
             NewRef = erlang:monitor(process, NewLeader),
             io:format("[~s] Remonitoring the New Leader (~w)~n", [Name, NewLeader]),
-            slave(Name, Master, NewLeader, NewSlaves, NewRef, Seq, Last);
+            slave(Name, Master, NewLeader, NewSlaves, NewRef, Seq, {view, NewLeader, NewSlaves, Seq});
         {'DOWN', _Ref, process, Leader, _Reason} ->
             io:format("[~s] Our Leader, ~w, is down. Making a new election~n", [Name, Leader]),
             election(Name, Master, Slaves, MonitorRef, N, Last);
